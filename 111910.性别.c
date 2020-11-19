99e2bc98600be3c87f6402d3724f1d24b9472947
rg203 <74295115+rg203@users.noreply.github.com>
2020-11-19 20:05:37 +0800

    Create 111910.性别.c

---
+++ 111910.性别.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+	int n;
+scanf("%d",&n);
+		if(n==0)
+		{
+printf("女\n",n);
+		}
+		else if(n==1)
+		{
+printf("男\n",n);
+		}
+		else
+		{
+printf("输出错误\n",n);
+		}
+}
