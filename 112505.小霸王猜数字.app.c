16425f7fec05945d019a6094f383ddf7bf6dc41f
rg203 <74295115+rg203@users.noreply.github.com>
2020-11-25 17:39:15 +0800

    Create 112505.小霸王猜数字.app.c

---
+++ 112505.小霸王猜数字.app.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<windows.h>
+void main(){
+int key=5,i;
+scanf("%d",&i);
+if(i==key){
+printf("right\n");}
+else if(i>key){
+printf("BIG\n");}
+else if(i<key){
+printf("SMALL\n");}
+
+}
