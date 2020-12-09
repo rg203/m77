867d07cbc30df7b7b616564af9c890339fca045a
rg203 <74295115+rg203@users.noreply.github.com>
2020-12-09 16:49:34 +0800

    Create 120904.串比对.app.c

---
+++ 120904.串比对.app.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+	char a1[]="LOVING U";
+	char a2[]="I LOVE U";
+if(strcmp(a1,a2)==0){
+		printf("same\n");
+	}
+else{
+printf("different\n");
+}
+}
