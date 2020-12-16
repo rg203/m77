261015c4e8e60bce8884277864c7b7aa8e6acf43
rg203 <74295115+rg203@users.noreply.github.com>
2020-12-16 09:27:49 +0800

    Update 121602.常规求和.app.c

--- 121602.常规求和.app.c
+++ 121602.常规求和.app.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
-int main(){
+int main()
+{
 int a;
 int i=1,sum=0;
 scanf("%d",&a);
-for(i=1;i<=a;i++){
-sum=sum+i;
-}
+ for(i=1;i<=a;i++)
+ {
+  sum=sum+i;
+ }
 printf("%d\n",sum);
 }
